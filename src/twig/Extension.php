eb9e951cb8e448f26d61ff8e7201448d6954ef14
Sebastian Lenz <github@sebastian-lenz.de>
2025-04-17 10:50:29 +0200

    Ensure we don't throw errors should shell_exec be unavailable

--- src/twig/Extension.php
+++ src/twig/Extension.php
@@ -83,7 +83,11 @@ class Extension extends AbstractExtension
     if (!isset($this->_commitHash)) {
       $this->_commitHash = ElementCache::with(self::CACHE_COMMIT, function() {
         try {
-          return substr(shell_exec('git rev-parse HEAD'), 0, 7);
+          if (!function_exists('shell_exec')) {
+            throw new Exception('Function shell_exec missing');
+          }
+
+          return substr(@shell_exec('git rev-parse HEAD'), 0, 7);
         } catch (Exception) {
           return '0000000';
         }
